7122d6dfb211b8d8a1631069ac2bb03774821b70
Bileonaire <kiokoleon@gmail.com>
2021-11-21 02:03:36 +0300

    Update deploy

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
   'surprise siege now sketch ready bubble garbage chuckle salt entire increase little',
@@ -13,10 +13,11 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
     .send({ gas: '1000000', from: accounts[0] });
 
+  console.log(abi);
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
